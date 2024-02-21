99dd6c65420d309922e7298d58d2d814b05e7ee5
Andrew Cortis <Andrew.Cortis@mcast.edu.mt>
2024-02-21 10:44:24 +0100

    Added RBAC and defalt admin

--- Securing_Applications_SWD62B_2023_24/Program.cs
+++ Securing_Applications_SWD62B_2023_24/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
@@ -40,5 +41,55 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+
+    if (roleManager == null || userManager == null)
+    {
+        // you have a problem with setting up the services
+        // Log this information
+        // Shutdown everything
+        return;
+    }
+
+    const string ADMIN_ROLE = "Admin";
+    const string DEFAULT_ADMIN_EMAIL = "defaultadmin@mysite.com";
+    const string DEFAULT_PASS = "SecretPass123!"; // Don't use passwords you find as examples!
+
+    bool adminExists = roleManager.RoleExistsAsync(ADMIN_ROLE).Result;
+
+    if (!adminExists)
+    {
+        roleManager.CreateAsync(new IdentityRole(ADMIN_ROLE)).Wait();
+    }
+
+    var adminUser = userManager.FindByEmailAsync(DEFAULT_ADMIN_EMAIL).Result;
+
+    if (adminUser == null) { // admin user does not exist
+        var createdAdminUserResult = userManager.CreateAsync(
+            new ApplicationUser() { UserName = DEFAULT_ADMIN_EMAIL, Email = DEFAULT_ADMIN_EMAIL },
+            DEFAULT_PASS).Result;
+    
+        if (!createdAdminUserResult.Succeeded)
+        {
+            // issue setting up admin
+            // Log this information
+            // Shutdown everything
+            return;
+        }
+
+        adminUser = userManager.FindByEmailAsync(DEFAULT_ADMIN_EMAIL).Result;
+    }
+
+    if (!userManager.IsInRoleAsync(adminUser, ADMIN_ROLE).Result)
+    {
+        var addIdentityRoleResult = userManager.AddToRoleAsync(adminUser, ADMIN_ROLE).Result;
+    }
+
+    
+}
 
 app.Run();
